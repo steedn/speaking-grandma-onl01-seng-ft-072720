bd145019e4ad263f7fcf3d6852223b28d0aabba4
Nathan Steed <steedn@outlook.com>
2020-07-27 22:44:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase != true
     return "HUH?! SPEAK UP, SONNY!"
-  else 
+  else
     return "NO! NOT SINCE 1938!"
   end
 end
