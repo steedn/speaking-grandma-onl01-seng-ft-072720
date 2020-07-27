6376c4d554e002351af87be9b6bd85bac9d73e90
Nathan Steed <steedn@outlook.com>
2020-07-27 22:44:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,10 @@
-require_relative
-# Write a speak_to_grandma method.
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-  elsif require_relative != phrase.upcase
-      return "HUH?! SPEAK UP, SONNY!"
-  else
-      return "NO, NOT SINCE 1938!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase != true
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NO! NOT SINCE 1938!"
+  end
 end
-
-speak_to_grandma
+    
