cdcf14bfe5a505fe268e277dd74cb7d22b266377
Nathan Steed <steedn@outlook.com>
2020-07-27 22:21:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,17 +5,11 @@
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
-    {
       puts "I LOVE YOU TOO PUMPKIN!"
-    }
   elsif phrase != phrase.upcase
-    {
       puts "HUH?! SPEAK UP, SONNY!"
-    }
   else phrase == phrase.upcase
-    {
       puts "NO, NOT SINCE 1938!"
-    }
   end
 
 # If you shout, she can hear you (or at least she thinks so)
